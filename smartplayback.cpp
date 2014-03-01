841c2dbaf78c106199df564339e5fd24c8546a9e
J-P Nurmi <jpnurmi@gmail.com>
2014-03-02 01:36:12 +0200

    Tidy up

--- smartplayback.cpp
+++ smartplayback.cpp
@@ -14,6 +14,8 @@
 #include <znc/znc.h>
 #include <sys/time.h>
 
+static const char* SmartPlaybackCap = "znc.in/smartplayback";
+
 class CSmartPlaybackMod : public CModule
 {
 public:
@@ -21,12 +23,33 @@ public:
 
     virtual void OnClientCapLs(CClient* pClient, SCString& ssCaps)
     {
-        ssCaps.insert("znc.in/smartplayback");
+        ssCaps.insert(SmartPlaybackCap);
     }
 
     virtual bool IsClientCapSupported(CClient* pClient, const CString& sCap, bool bState)
     {
-        return sCap.StartsWith("znc.in/smartplayback");
+        return sCap.Equals(SmartPlaybackCap);
+    }
+
+    virtual EModRet OnChanBufferStarting(CChan& Chan, CClient& Client)
+    {
+        if (Client.IsCapEnabled(SmartPlaybackCap))
+            return HALTCORE;
+        return CONTINUE;
+    }
+
+    virtual EModRet OnChanBufferPlayLine(CChan& Chan, CClient& Client, CString& sLine)
+    {
+        if (Client.IsCapEnabled(SmartPlaybackCap))
+            return HALTCORE;
+        return CONTINUE;
+    }
+
+    virtual EModRet OnChanBufferEnding(CChan& Chan, CClient& Client)
+    {
+        if (Client.IsCapEnabled(SmartPlaybackCap))
+            return HALTCORE;
+        return CONTINUE;
     }
 
     virtual EModRet OnUserRaw(CString& sLine)
@@ -34,44 +57,23 @@ public:
         CClient* pClient = GetClient();
         CIRCNetwork* pNetwork = GetNetwork();
         if (pClient && pNetwork && sLine.Token(0).Equals("PLAYBACK")) {
-            // PLAYBACK <chan(s)> [timestamp]
-            SCString ssArgs;
-            sLine.Token(1).Split(",", ssArgs, false);
+            // PLAYBACK <#chan(s)> [timestamp]
+            VCString vsArgs;
+            sLine.Token(1).Split(",", vsArgs, false);
 
             std::vector<CChan*> vChans;
-            for (SCString::const_iterator it = ssArgs.begin(); it != ssArgs.end(); ++it) {
-                std::vector<CChan*> vFound = FindChans(*it);
+            for (VCString::const_iterator it = vsArgs.begin(); it != vsArgs.end(); ++it) {
+                std::vector<CChan*> vFound = FindChans(pNetwork, *it);
                 vChans.insert(vChans.end(), vFound.begin(), vFound.end());
             }
 
             if (!vChans.empty()) {
                 time_t iSince = sLine.Token(2).ToLong();
                 for (std::vector<CChan*>::const_iterator it = vChans.begin(); it != vChans.end(); ++it)
-                    PutBuffer(pClient, (*it), iSince);
+                    PutBuffer(pClient, *it, iSince);
             }
             return HALTCORE;
         }
-        return CONTINUE;
-    }
-
-    virtual EModRet OnChanBufferStarting(CChan& Chan, CClient& Client)
-    {
-        if (Client.IsCapEnabled("znc.in/smartplayback"))
-            return HALTCORE;
-        return CONTINUE;
-    }
-
-    virtual EModRet OnChanBufferPlayLine(CChan& Chan, CClient& Client, CString& sLine)
-    {
-        if (Client.IsCapEnabled("znc.in/smartplayback"))
-            return HALTCORE;
-        return CONTINUE;
-    }
-
-    virtual EModRet OnChanBufferEnding(CChan& Chan, CClient& Client)
-    {
-        if (Client.IsCapEnabled("znc.in/smartplayback"))
-            return HALTCORE;
         return CONTINUE;
     }
 
@@ -79,7 +81,7 @@ public:
     {
         if (pClient && pClient->IsAttached() && pClient->HasServerTime()) {
             // TODO: proper handling for message tags (CUtils?)
-            if (pClient->IsCapEnabled("znc.in/smartplayback") && sLine.StartsWith(":")) {
+            if (pClient->IsCapEnabled(SmartPlaybackCap) && sLine.StartsWith(":")) {
                 timeval tv;
                 if (gettimeofday(&tv, NULL) == -1) {
                     tv.tv_sec = time(NULL);
@@ -92,7 +94,7 @@ public:
     }
 
 private:
-    void PutBuffer(CClient* pClient, const CChan* pChan, time_t iSince = 0)
+    static void PutBuffer(CClient* pClient, const CChan* pChan, time_t iSince = 0)
     {
         assert(pClient);
         assert(pChan);
@@ -113,18 +115,17 @@ private:
         }
     }
 
-    std::vector<CChan*> FindChans(const CString& sWild)
+    static std::vector<CChan*> FindChans(const CIRCNetwork* pNetwork, const CString& sWild)
     {
-        std::vector<CChan*> vFound;
-        const CIRCNetwork *pNetwork = GetNetwork();
-        if (pNetwork) {
-            const std::vector<CChan*>& vChans = pNetwork->GetChans();
-            for (std::vector<CChan*>::const_iterator it = vChans.begin(); it != vChans.end(); ++it) {
-                if ((*it)->GetName().WildCmp(sWild))
-                    vFound.push_back(*it);
-            }
+        assert(pNetwork);
+
+        std::vector<CChan*> vChans;
+        const std::vector<CChan*>& vAllChans = pNetwork->GetChans();
+        for (std::vector<CChan*>::const_iterator it = vAllChans.begin(); it != vAllChans.end(); ++it) {
+            if ((*it)->GetName().WildCmp(sWild))
+                vChans.push_back(*it);
         }
-        return vFound;
+        return vChans;
     }
 };
 
