d39053c1c52942799df7bab339e86e9a11c44bdb
J-P Nurmi <jpnurmi@gmail.com>
2014-03-02 13:56:26 +0200

    Tidy up OnSendToClient()

--- smartplayback.cpp
+++ smartplayback.cpp
@@ -79,17 +79,15 @@ public:
 
     virtual EModRet OnSendToClient(CClient* pClient, CString& sLine)
     {
-        if (pClient && pClient->IsAttached() && pClient->HasServerTime()) {
-            if (pClient->IsCapEnabled(SmartPlaybackCap) && sLine.StartsWith(":")) {
-                MCString mssTags = GetMessageTags(sLine);
-                timeval tv;
-                if (gettimeofday(&tv, NULL) == -1) {
-                    tv.tv_sec = time(NULL);
-                    tv.tv_usec = 0;
-                }
-                mssTags["time"] = FormatServerTime(tv);
-                SetMessageTags(sLine, mssTags);
+        if (pClient && pClient->IsAttached() && pClient->IsCapEnabled(SmartPlaybackCap)) {
+            timeval tv;
+            if (gettimeofday(&tv, NULL) == -1) {
+                tv.tv_sec = time(NULL);
+                tv.tv_usec = 0;
             }
+            MCString mssTags = GetMessageTags(sLine);
+            mssTags["time"] = FormatServerTime(tv);
+            SetMessageTags(sLine, mssTags);
         }
         return CONTINUE;
     }
